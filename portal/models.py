18ff77fc208840b6f25a87884a8b031d759c24a6
gfroosen <guido.f.roosen@gmail.com>
2016-03-28 12:32:30 +0100

    MbotDjango

---
+++ portal/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Robot(models.Model):
+
+    controler = models.ForeignKey('auth.User')
+    robot = models.CharField(max_length=200)
+    motorspeedright = models.FloatField(blank=True)
+    motorspeedleft = models.FloatField(blank=True)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.robot
