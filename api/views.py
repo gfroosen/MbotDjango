05e5ee60e96a79ca2880ec81a07eac4e95ec5e0c
gfroosen <guido.f.roosen@gmail.com>
2016-04-02 18:38:10 +0100

    added /edit

--- api/views.py
+++ api/views.py
@@ -1,14 +1,13 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from portal.models import Robot
 from api.serializers import RobotSerializer
-
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import PostForm
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 
 
@@ -72,7 +71,28 @@ def robot_control(request):
             post = form.save(commit=False)
             post.controler=request.user
             post.save()
-            
+
     else:
         form = PostForm()
     return render(request, 'api/robot_control.html', {'form': form})
+
+
+def robot_detail(request,pk):
+    form = get_object_or_404(Robot, pk=pk)
+
+    return render(request, 'api/robot_detail.html', {'form': form})
+
+def robot_edit(request, pk):
+    form = get_object_or_404(Robot, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=form)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.controler = request.user
+            post.published_date = timezone.now()
+            post.save()
+        return render(request, 'api/robot_detail.html', {'form': form})
+
+    else:
+        form = PostForm(instance=form)
+    return render(request, 'api/robot_edit.html', {'form': form})
